4df64afea75af54b65781aa6f48df9a3b6ce326d
Santiago Castillo <sancastillog@gmail.com>
2020-08-05 04:29:16 +0000

    task 4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Configure pass auth and auto key
+
+file_line { 'holberton'
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+  match => '^IdentityFile'
+}
+
+file_line { 'authentication'
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication'
+}
