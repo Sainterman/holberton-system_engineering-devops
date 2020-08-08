fa41c2eda2eb0916f91dcb9dcadaeef0a415e123
Santiago Castillo <Sainterman@users.noreply.github.com>
2020-08-08 18:35:53 -0500

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Configure pass auth and auto key
 
-file_line { 'holberton'
+file_line { 'holberton':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
   match  => '^IdentityFile',
 }
 
-file_line { 'authentication'
+file_line { 'authentication':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
