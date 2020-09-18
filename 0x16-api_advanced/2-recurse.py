79426547db3fdbb67e99cfb295dc84b90ed57a7c
Sainterman <sancastillog@gmail.com>
2020-09-17 19:12:23 -0500

    task 2:

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Get subscribers count from existing subreddit"""
+
+import requests
+
+
+def recurse(subreddit, after=None, hot_list=[]):
+    """ """
+    DOMAIN = 'https://www.reddit.com/'
+    USER_AGENT = 'linux:subredditscript:v1 (by /u/sancagar)'
+    h = {'User-Agent': USER_AGENT}
+    URL = DOMAIN + 'r/{}/hot.json'.format(subreddit)
+    params = dict()
+    if after:
+        URL += '?after={}'.format(after)
+    req = requests.get(URL, headers=h)
+    data = req.json().get('data')
+    try:
+        posts = data.get('children')
+    except KeyError:
+        return None
+    for post in posts:
+        hot_list.append(post.get('data')['title'])
+    if data['after']:
+        return recurse(subreddit, data['after'], hot_list)
+    else:
+        return hot_list
