83837a941dac53f89c723bf16f30cc7003fe6755
Santiago Castillo <sancastillog@gmail.com>
2020-10-09 03:00:41 +0000

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,4 @@
-# Set nofile for holberton to 10000
-
+# Set for holberton
 exec { 'user-limit':
      command => 'sed -i s/holberton/#holberton/ /etc/security/limits.conf',
      path    => '/bin/:/usr/bin/',
